53b29880a5adbbfdbb81828a0c2ca330198cf4de
Amit Singh <hshimanshu73@gmail.com>
2020-07-13 16:26:27 +0530

    initial

---
+++ BAL/Models/HeaderFieldModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BAL.Models
+{
+    public class HeaderFieldModel
+    {
+
+        public int iMessageImplementationId { get; set; }
+        public string sSenderID { get; set; }
+        public string sReceiverID { get; set; }
+        public string sVersionNo { get; set; }
+        public string sIndicator { get; set; }
+        public string sMessageID { get; set; }
+        public int? iSequenceOrControlNumber { get; set; }
+        public string sDate { get; set; }
+        public string sTime { get; set; }
+        public string sReportingEvent { get; set; }
+
+    }
+}
---
+++ SeaManifest/Controllers/MessageImplementationController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SeaManifest.Controllers
+{
+    public class MessageImplementationController : Controller
+    {
+        // GET: MessageImplementation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult AddHeader()
+        {
+            return PartialView("pvAddHeader");
+        }
+
+
+    }
+}
