3c3b5fcc812f3df8358d80226f89971d72d8b2f4
Himanshu Sharma <hshimanshu73@gmail.com>
2020-07-15 12:04:32 +0530

    d

--- SeaManifest/Scripts/PageScripts/Login.js
+++
@@ -1,9 +0,0 @@
-﻿$(document).ready(function () {
-    var $container = $("html,body");
-    var $scrollTo;
-    $scrollTo = $('.signin-form');
-    $container.animate({
-        scrollTop: $scrollTo.offset().top - 120,
-        scrollLeft: 0
-    }, 1000);
-});
